69666e6042bb784b4dc69d109fd2ca60bb2858fe
Lusan Sapkota <91797475+Lusan-sapkota@users.noreply.github.com>
2024-05-04 10:07:30 +0545

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+import urllib.parse
+from waitress import serve
+
+app = Flask(__name__)
+
+
+username = 'sapkotalusan'
+password = 'Lusan@7890'
+escaped_username = urllib.parse.quote_plus(username)
+escaped_password = urllib.parse.quote_plus(password)
+app.config['MONGO_URI'] = f'mongodb+srv://{escaped_username}:{escaped_password}@cluster0.aga31zx.mongodb.net/portfolio'
+
+mongo = PyMongo(app)
+
+app.secret_key = 'PuspaKunwar'
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/privacy')
+def privacy():
+    return render_template("privacy.html")
+
+@app.route('/terms')
+def terms():
+    return render_template("terms.html")
+
+
+@app.route('/submit_contact_form', methods=['POST'])
+def submit_contact_form():
+    try:
+        # Access form data
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        country = request.form['country']
+        message = request.form['message']
+
+        # Insert data into MongoDB
+        mongo.db.user_data.insert_one({
+            'name': name,
+            'email': email,
+            'phone': phone,
+            'country': country,
+            'message': message
+        })
+
+        # Optionally, redirect the user to a thank you page
+        return render_template('thank_you.html')
+
+    except Exception as e:
+        # Handle the error
+        print("An error occurred:", str(e))
+        return "An error occurred while processing your request. Please try again later.", 500
+
+mode = "development"
+
+
+if __name__ == "__main__":
+    if mode == "production":
+        serve(app, host="0.0.0.0", port=5000)
+    else:
+        app.run(debug=True, host="0.0.0.0", port=5000)
